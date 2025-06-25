5f61c656aa3375ff726a0f396739273c8bd664b5
M-giraffe <115019651+nhatminh280@users.noreply.github.com>
2025-06-26 05:01:47 +0700

    Update tmp.cpp

--- tmp.cpp
+++ tmp.cpp
@@ -11,81 +11,32 @@
 #define FOD(i, a, b) for (int i = (a); i >= (b); i--)
 #define REP(i, n) for (int i = 0; i < (n); i++)
 #define ALL(x) (x).begin(), (x).end()
-#define nl cout << "/n"; 
-using ll = long long; 
+#define nl cout << "/n";
+
+using ll = long long;
 using lli = long long int;
 using ulli = unsigned long long int;
 using ld = long double;
 
 const int INF = 1e9;
 const int MAXN = 2e5+5;
-
-const lli LLINF = 4*1e18;
-const lli MOD = 1e9+7;
-                                                        /*M_griffe*/
-
-#pragma GCC optimize("O2")
-#pragma GCC target("avx,avx2,fma")
-#include <bits/stdc++.h>
-
-#define pb push_back
-#define mp make_pair
-
-#define FOR(i, a, b) for (int i = (a); i <= (b); i++)
-#define FOD(i, a, b) for (int i = (a); i >= (b); i--)
-#define REP(i, n) for (int i = 0; i < (n); i++)
-#define ALL(x) (x).begin(), (x).end()
-#define nl cout << "/n"; 
-using ll = long long; 
-using lli = long long int;
-using ulli = unsigned long long int;
-using ld = long double;
-
-const int INF = 1e9;
-const int MAXN = 2e5+5;
-
 const lli LLINF = 4*1e18;
 const lli MOD = 1e9+7;
 const ld PI = 3.141592653589793;
 
-
 using namespace std;
+
 void solve()
 {
-    
+    ${1:// your code here}
 }
 
 signed main() {
     ios::sync_with_stdio(false);
     cout.tie(0); cin.tie(0);
-    ll test=1;
-   //cin>>test;
-    while(test--)
-    {
+    ll test = ${2:1};
+    // cin >> test;
+    while(test--) {
         solve();
     }
     return 0;
-
-}
-
-const ld PI = 3.141592653589793;
-
-
-using namespace std;
-void solve()
-{
-    // your code here 
-}
-
-signed main() {
-    ios::sync_with_stdio(false);
-    cout.tie(0); cin.tie(0);
-    ll test;
-   cin>>test;
-    while(test--)
-    {
-        solve();
-    }
-    return 0;
-
-}
